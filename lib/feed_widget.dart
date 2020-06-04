18b7a380c32e8bb943e751136ff07323fb19e040
KimRyeongHyeon <52704419+KimRyeongHyeon@users.noreply.github.com>
2020-06-04 16:41:09 +0900

    댓글 표시 UI 살펴보기

--- lib/feed_widget.dart
+++ lib/feed_widget.dart
@@ -26,7 +26,7 @@ class _FeedWidgetState extends State<FeedWidget> {
 
   @override
   Widget build(BuildContext context) {
-    var comment = widget.document['comment'] ?? 0;
+    var commentCount = widget.document['commentCount'] ?? 0;
     return Column(
       children: <Widget>[
         ListTile(
@@ -100,33 +100,34 @@ class _FeedWidgetState extends State<FeedWidget> {
         SizedBox(
           height: 8.0,
         ),
-        GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => CommentPage(widget.document),
-              ),
-            );
-          },
-          child: Padding(
-            padding: const EdgeInsets.only(left: 16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Text(
-                      '댓글 $comment개 모두 보기',
-                      style: TextStyle(color: Colors.grey[500]),
-                    ),
-                  ],
+        if (commentCount > 0)
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CommentPage(widget.document),
                 ),
-                Text(widget.document['lastComment'] ?? ''),
-              ],
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        '댓글 $commentCount개 모두 보기',
+                        style: TextStyle(color: Colors.grey[500]),
+                      ),
+                    ],
+                  ),
+                  Text(widget.document['lastComment'] ?? ''),
+                ],
+              ),
             ),
           ),
-        ),
         Row(
           children: <Widget>[
             Expanded(
@@ -173,7 +174,7 @@ class _FeedWidgetState extends State<FeedWidget> {
   void _unlike() {
     // 기존 좋아요 리스트(배열)로 복사
     final List likedUsers =
-    List<String>.from(widget.document['likedUsers'] ?? []);
+        List<String>.from(widget.document['likedUsers'] ?? []);
 
     // 나를 추가
     likedUsers.remove(widget.user.email);
